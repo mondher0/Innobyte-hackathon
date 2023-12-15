43a8719a90a24cc7feba3c524ef0821ed3e4d534
YoucefAllaoua <y_allaoua@estin.dz>
2023-12-15 21:06:00 +0100

    fixing  offers sectoin in the loggede user page

--- components/user/OffersContainer.jsx
+++ components/user/OffersContainer.jsx
@@ -25,28 +25,6 @@ const OffersContainer = () => {
       isDisliked: false,
       boJob: "CEO at Hotel",
     },
-    {
-      postulationDate: "posted 3  days ago",
-      title: "React Developer",
-      description: "Creating A website For my Hotel so can manage all the employees of the hotel, also i can manage bookings and all the necessary stuff.",
-      positions: ["React Developer", "Nodejs Developer", "UI/UX Designer"],
-      businessOwner: "Allaoua Youcef",
-      boImage: "https://picsum.photos/200/300",
-      isLiked: true,
-      isDisliked: false,
-      boJob: "CEO at Hotel",
-    },
-    {
-      postulationDate: "posted 3  days ago",
-      title: "React Developer",
-      description: "Creating A website For my Hotel so can manage all the employees of the hotel, also i can manage bookings and all the necessary stuff.",
-      positions: ["React Developer", "Nodejs Developer", "UI/UX Designer"],
-      businessOwner: "Allaoua Youcef",
-      boImage: "https://picsum.photos/200/300",
-      isLiked: true,
-      isDisliked: false,
-      boJob: "CEO at Hotel",
-    },
   ];
   return (
     <div className=" flex  justify-between flex-wrap gap-7 pb-10 pt-24 w-full    ">
