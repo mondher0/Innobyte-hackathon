f22faf363b46d82a983ce17ea3f5c618fecaa548
mondher <110062316+mondher0@users.noreply.github.com>
2023-12-16 10:58:24 +0100

    adding navigations between pages

--- app/page.jsx
+++ app/page.jsx
@@ -1,95 +1,21 @@
+import EcoSystemSection from "@/components/home/EcoSystemSection";
+import HomeHero from "@/components/home/HomeHero";
+import SolutionsSection from "@/components/home/SolutionsSection";
+import Work from "@/components/home/Work";
+import Header from "@/components/ui/Header";
+import HeroSection from "@/components/user/HeroSection";
+import React from "react";
 
-"use client";
-import {
-  receiveMessage,
-  resetCurrentMessage,
-  sendMessage,
-  setCurrentMessage,
-  setReceiveMessage,
-  socket,
-} from "@/store/features/chat/chat-slice";
-import { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { io } from "socket.io-client";
-// const socket = io("wss://moisaic.onrender.com/", {
-//   transports: ["websocket"],
-//   auth: {
-//     authorization: "Bearer " + "localS",
-//   },
-// });
-export default function Home() {
-  const { currentMessage, messages } = useSelector((state) => state.chat);
-  console.log(messages);
-  const dispatch = useDispatch();
-
-  // const sendMessage = async () => {
-  //   if (currentMessage !== "") {
-  //     const messageData = {
-  //       message: currentMessage,
-  //     };
-
-  //     socket.emit("send-message", messageData);
-  //     console.log("message sent");
-  //     setCurrentMessage("");
-  //   }
-  // };
-
-  useEffect(() => {
-    // dispatch(receiveMessage());
-
-    socket.on("receive-message", (data) => {
-      console.log("message received");
-      console.log(data);
-      dispatch(setReceiveMessage(data));
-    });
-    return () => {
-      socket.off("receive-message");
-    };
-  }, [socket]);
-
+const HomePage = () => {
   return (
     <>
-      <h1>home page</h1>
-      <div className="chat-window">
-        <div className="chat-header">
-          <p>Live Chat</p>
-        </div>
-        <div className="chat-body">
-          {messages.map((val, key) => {
-            return (
-              <div
-                key={key}
-                className="chat-message"
-                style={{
-                  backgroundColor:
-                    val?.message === currentMessage ? "#F0F0F0" : "#FFF",
-                }}
-              >
-                <p>{val?.message ? val?.message : val}</p>
-              </div>
-            );
-          })}
-        </div>
-        <div className="chat-footer">
-          <input
-            type="text"
-            placeholder="Hey..."
-            onChange={(e) => {
-              dispatch(setCurrentMessage(e.target.value));
-            }}
-            value={currentMessage}
-          />
-          <button
-            onClick={() => {
-              dispatch(sendMessage(currentMessage));
-              dispatch(resetCurrentMessage(currentMessage));
-            }}
-          >
-            Send
-          </button>
-        </div>
-      </div>
+      <Header />
+      <HomeHero />
+      <EcoSystemSection />
+      <SolutionsSection />
+      <Work />
     </>
   );
-}
+};
 
+export default HomePage;
--- components/ui/Header.jsx
+++ components/ui/Header.jsx
@@ -6,6 +6,8 @@ import Link from "next/link";
 import IconProvider from "./IconProvider";
 
 import AccountDropDown from "../user/AccountDropDown";
+import { useParams, usePathname, useRouter } from "next/navigation";
+import { Button } from "./button";
 const Header = () => {
   const links = [
     {
@@ -16,7 +18,18 @@ const Header = () => {
       text: "Support",
       link: "",
     },
+    {
+      text: "For Freelancers",
+      link: "",
+    },
+    {
+      text: "About",
+      link: "",
+    },
   ];
+  const pathName = usePathname();
+  console.log(pathName);
+  const router = useRouter();
   return (
     <header className=" flex items-center justify-between ">
       {/* this is the right section */}
@@ -26,6 +39,8 @@ const Header = () => {
             src={"/Logo.svg"}
             width={130}
             height={30}
+            onClick={() => router.push("/")}
+            className="hover:cursor-pointer"
           />
           <VersionBadge />
         </div>
@@ -33,26 +48,35 @@ const Header = () => {
         <ul className="links flex items-center gap-10 list-none">
           {links.map((link, index) => (
             <li key={index}>
-              <Link
-                href={link.link}
-                className=" text-grey600  text-sm font-medium duration-300 hover:underline "
-              >
-                {link.text}
-              </Link>
+              {pathName === "/" &&
+              link.text === "Collaboration Space" ? null : (
+                <Link href={link.link}>{link.text}</Link>
+              )}
             </li>
           ))}
         </ul>
       </div>
       {/*  this si the right section */}
-      <div className="right-section flex items-center gap-6 ">
-        {/* if  the user is logged in */}
-        <Link href={""}>
-          <IconProvider>
-            <MdOutlineNotificationsNone size={25} />
-          </IconProvider>
-        </Link>
-        <AccountDropDown />
-      </div>
+      {pathName === "/" || pathName === "/login" || pathName === "/register" ? (
+        <div className="flex items-center gap-3">
+          <Button>
+            <Link href={"/login"}>Login</Link>
+          </Button>
+          <Button variant="soft">
+            <Link href={"/register"}>Register</Link>
+          </Button>
+        </div>
+      ) : (
+        <div className="right-section flex items-center gap-6 ">
+          {/* if  the user is logged in */}
+          <Link href={""}>
+            <IconProvider>
+              <MdOutlineNotificationsNone size={25} />
+            </IconProvider>
+          </Link>
+          <AccountDropDown />
+        </div>
+      )}
     </header>
   );
 };
